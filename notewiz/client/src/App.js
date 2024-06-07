06539144a84d75b537ed4f5a021f1ceb7d78c4ce
RIckxixixi <xuchen.zhang@mail.utoronto.ca>
2024-06-07 18:10:03 -0400

    Markdown components
    update client Markdown components
    
    test version

---
+++ notewiz/client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import CreateNote from './components/CreateNote';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Notewiz Application</h1>
+      <CreateNote />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
