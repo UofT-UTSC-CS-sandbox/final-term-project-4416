1c6a5e6db3f9d2858c4d2b6ea46061ee57fd9c15
RIckxixixi <xuchen.zhang@mail.utoronto.ca>
2024-06-07 15:37:02 -0400

    Set up
    set up client and part of server

---
+++ notewiz/server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Connect MongoDB
+mongoose.connect('mongodb://localhost:27017/notewiz')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+// Simple Route
+app.get('/', (req, res) => {
+  res.send('Notewiz API');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
