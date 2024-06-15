5f111fab38e90f1d70ac92fa5066c46c178f2594
RIckxixixi <xuchen.zhang@mail.utoronto.ca>
2024-06-14 20:48:12 -0400

    Revert "Merge branch 'dev' of https://github.com/UofT-UTSC-CS-sandbox/final-term-project-4416 into dev"
    This reverts commit f6c9450cf5d2ffc367c2e0790bd704a1baaee338, reversing
    changes made to 13b35f0cdb75d56eb520067f4d4ce7b4889adf39.

--- notewiz/client/src/App.js
+++ notewiz/client/src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import './App.css';
 import CreateNote from './components/CreateNote';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login/>}></Route>
-        <Route path='/signup' element={<Signup/>}></Route>
-        <Route path='/Note' element={<CreateNote/>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <h1>Notewiz Application</h1>
+      <CreateNote />
+    </div>
   );
 }
 
